4004a478a69df98f18130ec1bffb667ba48c1bc2
Jennifer <jennyangeleshernandez310@gmail.com>
2024-04-10 08:53:44 -0600

    ADD:Routing en Laravel definir la nevegacion del sitio web para venta, contacto y nosotros

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/nuestra-tienda', function () {
+    return view('nosotros');
+});
+Route::get('/tienda-laptos', function () {
+    return view('venta');
+});
+Route::get('/contactanos', function () {
+    return view('contacto');
+});
